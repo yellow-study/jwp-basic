8fa0291c230e7ac6ded5c466c28d1cd277bfdc88
sunyoung-dev <sunyoung-dev@gmail.com>
2019-11-19 21:39:12 +0900

    7.1 회원 목록 조회 및 개인정보 수정

--- src/main/java/next/controller/ListUserController.java
+++ src/main/java/next/controller/ListUserController.java
@@ -1,19 +1,21 @@
 package next.controller;
 
+import core.mvc.Controller;
+import next.dao.UserDao;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import core.db.DataBase;
-import core.mvc.Controller;
-
 public class ListUserController implements Controller {
+    private UserDao userDao = new UserDao();
+
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         if (!UserSessionUtils.isLogined(req.getSession())) {
             return "redirect:/users/loginForm";
         }
 
-        req.setAttribute("users", DataBase.findAll());
+        req.setAttribute("users", userDao.findAll());
         return "/user/list.jsp";
     }
 }
