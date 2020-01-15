c0dd0581df58b3d6efb5d4c34bd4d7ec3bcafa78
nt11236 <nt11236@nts-corp.com>
2020-01-15 14:54:54 +0900

    - 질문 추가까지 완료

--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -1,10 +1,40 @@
-String.prototype.format = function() {
-  var args = arguments;
-  return this.replace(/{(\d+)}/g, function(match, number) {
-    return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-        ;
-  });
+String.prototype.format = function () {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function (match, number) {
+        return typeof args[number] != 'undefined'
+            ? args[number]
+            : match
+            ;
+    });
 };
 
+$(function () {
+    $("form[name='answer']").find("input[type='submit']").click(addAnswer);
+});
+
+function addAnswer() {
+    event.preventDefault();
+    var data = $("form[name='answer']").serialize();
+
+    console.log(data);
+
+    $.ajax({
+        url: "/qna/addAnswer"
+        , data: data
+        , method : "POST"
+        , dataType : "json"
+        , success : onSuccess
+        , error : onError
+    })
+}
+
+function onSuccess(result) {
+    var articleTemplate = jQuery("#answerTemplate").html();
+    var template = articleTemplate.format(result.writer, new Date(result.createdDate), result.contents, result.answerId);
+
+    $(".qna-comment-slipp-articles").prepend(template)
+}
+
+function onError() {
+
+}
