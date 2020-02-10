0ce30a670be49d0815b5d1ca0c1d2111afabd77e
younghyun.um <uyh1023@nts-corp.com>
2020-02-10 19:07:12 +0900

    [9.1.4] 로그인 사용자만 질문 가능하도록 수정

--- src/main/java/next/controller/qna/AddQuestionController.java
+++ src/main/java/next/controller/qna/AddQuestionController.java
@@ -1,30 +1,39 @@
 package next.controller.qna;
 
-import core.mvc.AbstractController;
-import core.mvc.ModelAndView;
-import core.web.filter.Controller;
-import next.dao.QuestionDao;
-import next.model.Question;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import core.mvc.AbstractController;
+import core.mvc.ModelAndView;
+import core.web.filter.Controller;
+import next.controller.UserSessionUtils;
+import next.dao.QuestionDao;
+import next.model.Question;
+import next.model.User;
+
 @Controller("/qna/create")
 public class AddQuestionController extends AbstractController {
-    private static final Logger log = LoggerFactory.getLogger(AddQuestionController.class);
+	private static final Logger log = LoggerFactory.getLogger(AddQuestionController.class);
 
-    private QuestionDao questionDao = new QuestionDao();
+	private QuestionDao questionDao = new QuestionDao();
 
-    @Override
-    public ModelAndView execute(HttpServletRequest req, HttpServletResponse response) throws Exception {
-        Question question = new Question(req.getParameter("writer"), req.getParameter("title")
-                , req.getParameter("contents"));
-        log.debug("question : {}", question);
+	@Override
+	public ModelAndView execute(HttpServletRequest req, HttpServletResponse response) throws Exception {
 
-        questionDao.insert(question);
+		if (!UserSessionUtils.isLogined(req.getSession())) {
+			return jspView("redirect:/users/loginForm");
+		}
 
-        return jspView("redirect:/");
-    }
+		User user = UserSessionUtils.getUserFromSession(req.getSession());
+		Question question = new Question(user.getName(), req.getParameter("title")
+			, req.getParameter("contents"));
+		log.debug("question : {}", question);
+
+		questionDao.insert(question);
+
+		return jspView("redirect:/");
+	}
 }
