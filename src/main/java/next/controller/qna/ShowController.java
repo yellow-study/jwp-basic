203c4dc505c31f0084264186d579abaf87455178
umyonghyeon <uyh1023@naver.com>
2020-02-16 17:24:34 +0900

    [9.1.7]showController multi thread issue resolved

--- src/main/java/next/controller/qna/ShowController.java
+++ src/main/java/next/controller/qna/ShowController.java
@@ -5,29 +5,28 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import core.mvc.AbstractController;
+import core.mvc.ModelAndView;
 import next.dao.AnswerDao;
 import next.dao.QuestionDao;
 import next.model.Answer;
 import next.model.Question;
-import core.mvc.AbstractController;
-import core.mvc.ModelAndView;
 
 public class ShowController extends AbstractController {
-    private QuestionDao questionDao = new QuestionDao();
-    private AnswerDao answerDao = new AnswerDao();
-    private Question question;
-    private List<Answer> answers;
+	private QuestionDao questionDao = new QuestionDao();
+	private AnswerDao answerDao = new AnswerDao();
 
-    @Override
-    public ModelAndView execute(HttpServletRequest req, HttpServletResponse response) throws Exception {
-        Long questionId = Long.parseLong(req.getParameter("questionId"));
+	@Override
+	public ModelAndView execute(HttpServletRequest req, HttpServletResponse response) throws Exception {
+		Long questionId = Long.parseLong(req.getParameter("questionId"));
 
-        question = questionDao.findById(questionId);
-        answers = answerDao.findAllByQuestionId(questionId);
+		Question question = questionDao.findById(questionId);
+		List<Answer> answers = answerDao.findAllByQuestionId(questionId);
 
-        ModelAndView mav = jspView("/qna/show.jsp");
-        mav.addObject("question", question);
-        mav.addObject("answers", answers);
-        return mav;
-    }
+		ModelAndView mav = jspView("/qna/show.jsp");
+		mav.addObject("question", question);
+		mav.addObject("answers", answers);
+
+		return mav;
+	}
 }
